c27238f8d2859c6bd4bb4670cf01215b6de48fcb
theiago <theiago@live.com>
2022-10-19 21:27:10 -0300

    ola

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const data = require("./db.js");
-
-const jsonServer = require("json-server");
+const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router(data);
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
+const path = '/api/v1';
 
+server.listen()
 server.use(middlewares);
-server.use(router);
+server.use(path, router);
 
 server.listen(port);
